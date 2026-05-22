87b48cf1fc5491f0495a0c7171cc7b1702a58f91
KristinaFinck <kristina.finck83@gmail.com>
2026-05-22 23:55:47 +0200

    feat(playlists): add react-toastify

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -2,7 +2,7 @@ import '../../App.css'
 import {Routing} from "@/common/routing";
 import {Header} from "@/common/components/Header/Header.tsx";
 import s from "./App.module.css";
-
+import { ToastContainer } from 'react-toastify'
 function App() {
 
 
@@ -12,6 +12,7 @@ function App() {
         <div className={s.layout}>
       <Routing/>
         </div>
+        <ToastContainer />
     </>
   )
 }
