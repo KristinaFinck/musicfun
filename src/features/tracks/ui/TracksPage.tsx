8c3cb53b1ed32d198bc0e256519cdf47dd2d285e
KristinaFinck <kristina.finck83@gmail.com>
2026-06-23 22:32:09 +0200

    chore: fix imports and add page styles

--- src/features/tracks/ui/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage.tsx
@@ -1,12 +1,29 @@
-import {useFetchTracksInfiniteQuery} from "@/features/tracks/api/trackApi.ts";
+import { useFetchTracksInfiniteQuery } from './../api/trackApi.ts'
+import s from './../ui/TrackPage.module.css'
 
 export const TracksPage = () => {
-    const { data } = useFetchTracksInfiniteQuery()
+    const { data } = useFetchTracksInfiniteQuery({ paginationType: 'cursor', pageSize: 5 })
 
-    console.log(data)
+    const pages = data?.pages.flatMap((page) => page.data) || []
+
     return (
         <div>
-            <h1> Tracks page </h1>
+            <h1>Tracks page</h1>
+            <div className={s.list}>
+                {pages.map(track => {
+                    const { title, user, attachments } = track.attributes
+
+                    return (
+                        <div key={track.id} className={s.item}>
+                            <div>
+                                <p>Title: {title}</p>
+                                <p>Name: {user.name}</p>
+                            </div>
+                            {attachments.length ? <audio controls src={attachments[0].url} /> : 'no file'}
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
