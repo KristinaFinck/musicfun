6f6fcfa854b08fec65bec9edaad7ef5ef69a24d7
KristinaFinck <kristina.finck83@gmail.com>
2026-06-27 23:43:51 +0200

    feat: add linear progress indicator during loading

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -6,16 +6,20 @@ export const baseApi = createApi({
     refetchOnFocus: true,
     refetchOnReconnect: true,
     keepUnusedDataFor: 5,
-    baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_BASE_URL,
-        headers: {
-            'API-KEY': import.meta.env.VITE_API_KEY,
-        },
+    baseQuery: async (args, api, extraOptions) => {
+        await new Promise(resolve => setTimeout(resolve, 2000)) // delay
+
+        return fetchBaseQuery({
+            baseUrl: import.meta.env.VITE_BASE_URL,
+            headers: {
+                'API-KEY': import.meta.env.VITE_API_KEY,
+            },
 
         prepareHeaders: headers => {
             headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
             return headers
         },
-    }),
+        })(args, api, extraOptions)
+    },
     endpoints: () => ({}),
 })
--- src/features/playlists/ui/PlayListsPage/PlaylstsPage.tsx
+++ src/features/playlists/ui/PlayListsPage/PlaylstsPage.tsx
@@ -7,6 +7,7 @@ import {ChangeEvent, useState} from "react";
 import {useDebounceValue} from "@/common/hooks/useDebounceValue.ts";
 import {Pagination} from "@/common/components/Pagination/Pagination.tsx";
 import {PlaylistsList} from "@/features/playlists/ui/PlaylistsList/PlaylistsList.tsx";
+import {LinearProgress} from "@/common/components/LinearProgress/LinearProgress.tsx";
 
 
 
@@ -17,12 +18,12 @@ export const PlaylistsPage = () => {
     const [search, setSearch] = useState('')
     const debounceSearch = useDebounceValue(search)
 
-    const { data, isLoading } = useFetchPlaylistsQuery({
+    const { data, isLoading, isFetching } = useFetchPlaylistsQuery({
         search: debounceSearch,
         pageNumber: currentPage,
         pageSize,
     })
-
+    console.log({ isLoading, isFetching })
     const changePageSizeHandler = (size: number) => {
         setPageSize(size)
         setCurrentPage(1)
@@ -32,7 +33,7 @@ export const PlaylistsPage = () => {
         setSearch(e.currentTarget.value)
         setCurrentPage(1)
     }
-
+    if (isLoading) return <h1>Skeleton loader...</h1>
     return (
         <div className={s.container}>
             <h1>Playlists page</h1>
@@ -43,6 +44,7 @@ export const PlaylistsPage = () => {
                 onChange={searchPlaylistHandler}
             />
             <PlaylistsList playlists={data?.data || []} isPlaylistsLoading={isLoading} />
+            {isFetching && <LinearProgress />}
             <Pagination
                 currentPage={currentPage}
                 setCurrentPage={setCurrentPage}
@@ -53,96 +55,6 @@ export const PlaylistsPage = () => {
         </div>
     )
 }
-//
-// export const PlaylistsPage = () => {
-//     const [playlistId, setPlaylistId] = useState<string | null>(null)
-//     const [search, setSearch] = useState('')
-//     const [currentPage, setCurrentPage] = useState(1)
-//     const [pageSize, setPageSize] = useState(2)
-//     const debounceSearch = useDebounceValue(search)
-//     const { register, handleSubmit, reset } = useForm<UpdatePlaylistArgs>()
-//
-//     const { data, isLoading } = useFetchPlaylistsQuery({
-//         search: debounceSearch,
-//         pageNumber: currentPage,
-//         pageSize,
-//     })
-//
-//     const [deletePlaylist] = useDeletePlaylistMutation()
-//
-//     const deletePlaylistHandler = (playlistId: string) => {
-//         if (confirm('Are you sure you want to delete the playlist?')) {
-//             deletePlaylist(playlistId)
-//         }
-//     }
-//
-//     const editPlaylistHandler = (playlist: PlaylistData | null) => {
-//         if (playlist) {
-//             setPlaylistId(playlist.id)
-//             reset({
-//                 title: playlist.attributes.title,
-//                 description: playlist.attributes.description,
-//                 tagIds: playlist.attributes.tags.map(t => t.id),
-//             })
-//         } else {
-//             setPlaylistId(null)
-//         }
-//     }
-//     const changePageSizeHandler = (size: number) => {
-//         setPageSize(size)
-//         setCurrentPage(1)
-//     }
-//     const searchPlaylistHandler = (e: ChangeEvent<HTMLInputElement>) => {
-//         setSearch(e.currentTarget.value)
-//         setCurrentPage(1)
-//     }
-//     return (
-//         <div className={s.container}>
-//             <h1>Playlists page</h1>
-//             <CreatePlaylistForm />
-//             <input
-//                 type="search"
-//                 placeholder={'Search playlist by title'}
-//                 onChange={searchPlaylistHandler}
-//             />
-//             <div className={s.items}>
-//                 {!data?.data.length && !isLoading && <h2>Playlists not found</h2>}
-//
-//                 {data?.data.map(playlist => {
-//                     const isEditing = playlistId === playlist.id
-//
-//                     return (
-//                         <div className={s.item} key={playlist.id}>
-//                             {isEditing ? (
-//                                 <EditPlaylistForm
-//                                     playlistId={playlistId}
-//                                     handleSubmit={handleSubmit}
-//                                     register={register}
-//                                     editPlaylist={editPlaylistHandler}
-//                                     setPlaylistId={setPlaylistId}
-//                                 />
-//                             ) : (
-//                                 <PlaylistItem
-//                                     playlist={playlist}
-//                                     deletePlaylist={deletePlaylistHandler}
-//                                     editPlaylist={editPlaylistHandler}
-//                                 />
-//                             )}
-//                         </div>
-//                     )
-//                 })}
-//             </div>
-//
-//                     <Pagination
-//                         currentPage={currentPage}
-//                         setCurrentPage={setCurrentPage}
-//                         pagesCount={data?.meta.pagesCount || 1}
-//                         pageSize={pageSize}
-//                         changePageSize={changePageSizeHandler}
-//                     />
-//                 </div>
-//     )
-//
-// }
+
 
 
