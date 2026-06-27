585c1d83dc1a9589af35b2ee2f90517643486206
KristinaFinck <kristina.finck83@gmail.com>
2026-06-28 00:44:16 +0200

    feat: implement global loading state

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -3,12 +3,16 @@ import {Routing} from "@/common/routing";
 import {Header} from "@/common/components/Header/Header.tsx";
 import s from "./App.module.css";
 import { ToastContainer } from 'react-toastify'
-function App() {
+import {useGlobalLoading} from "@/common/hooks/useGlobalLoading.ts";
+import {LinearProgress} from "@/common/components/LinearProgress/LinearProgress.tsx";
 
+function App() {
+    const isGlobalLoading = useGlobalLoading()
 
   return (
     <>
         <Header/>
+        {isGlobalLoading && <LinearProgress />}
         <div className={s.layout}>
       <Routing/>
         </div>
