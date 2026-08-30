62045f27411874157a4e406dce69fe3fea7529ce
KristinaFinck <kristina.finck83@gmail.com>
2026-08-30 21:38:59 +0200

    refactor: extract base query and update auth error handling

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import {handleErrors} from "@/common/utils";
 import {AUTH_KEYS} from "@/common/constants/constants.ts";
+import {baseQueryWithReauth} from "@/app/baseQueryWithReauth.ts";
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
@@ -8,29 +9,5 @@ export const baseApi = createApi({
     refetchOnFocus: true,
     refetchOnReconnect: true,
     keepUnusedDataFor: 5,
-    baseQuery: async (args, api, extraOptions) => {
-      //  await new Promise(resolve => setTimeout(resolve, 2000)) // delay
-        const result = await fetchBaseQuery({
-            baseUrl: import.meta.env.VITE_BASE_URL,
-            headers: {
-                'API-KEY': import.meta.env.VITE_API_KEY,
-            },
-
-        prepareHeaders: headers => {
-            const accessToken = localStorage.getItem(AUTH_KEYS.accessToken)
-            if (accessToken) {
-                headers.set('Authorization', `Bearer ${accessToken}`)
-            }
-            return headers
-        },
-
-        })(args, api, extraOptions)
-
-    if ('error' in result && result.error) {
-        handleErrors(result.error)
-}
-
-return result
-},
-    endpoints: () => ({}),
+    baseQuery: baseQueryWithReauth,
 })
