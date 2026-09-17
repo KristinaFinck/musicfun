aeb1e41c1eb3f7d69ec78a2caa14e650af30e497
KristinaFinck <kristina.finck83@gmail.com>
2026-09-17 12:01:25 +0200

    refactor(playlists): add Zod validation to CreatePlaylistForm

--- src/features/playlists/model/playlists.schemas.ts
+++ src/features/playlists/model/playlists.schemas.ts
@@ -4,6 +4,7 @@ import * as z from 'zod'
 export const createPlaylistSchema = z.object({
     title: z
         .string()
+        .trim()
         .min(1, 'The title length must be more than 1 character')
         .max(100, 'The title length must be less than 100 characters'),
     description: z.string().max(1000, 'The description length must be less than 1000 characters.'),
--- src/features/playlists/ui/CreatePlaylistForm/CreatePlaylistForm.tsx
+++ src/features/playlists/ui/CreatePlaylistForm/CreatePlaylistForm.tsx
@@ -1,13 +1,26 @@
-import {SubmitHandler, useForm} from "react-hook-form";
-import type {CreatePlaylistArgs} from "@/features/playlists/api/playlistsApi.types.ts";
+/*...*/
+import { type SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import s from './CreatePlaylistForm.module.css'
+
+import {createPlaylistSchema} from "@/features/playlists/model/playlists.schemas.ts";
 import {useCreatePlaylistMutation} from "@/features/playlists/api/playlistsApi.ts";
+import  type {CreatePlaylistArgs} from "@/features/playlists/api/playlistsApi.types.ts";
 
 export const CreatePlaylistForm = () => {
-    const { register, handleSubmit, reset } = useForm<CreatePlaylistArgs>()
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm<CreatePlaylistArgs>({
+        resolver: zodResolver(createPlaylistSchema),
+    })
     const [createPlaylist] = useCreatePlaylistMutation()
     const onSubmit: SubmitHandler<CreatePlaylistArgs> = data => {
-        createPlaylist(data)
-        reset()
+        createPlaylist(data).then(() => {
+            reset()
+        })
     }
 
     return (
@@ -15,9 +28,11 @@ export const CreatePlaylistForm = () => {
             <h2>Create new playlist</h2>
             <div>
                 <input {...register('title')} placeholder={'title'} />
+                {errors.title && <span className={s.error}>{errors.title.message}</span>}
             </div>
             <div>
                 <input {...register('description')} placeholder={'description'} />
+                {errors.description && <span className={s.error}>{errors.description.message}</span>}
             </div>
             <button>create playlist</button>
         </form>
